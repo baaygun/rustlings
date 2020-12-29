dc167aaba19e5288576651eb2a196e5b82108575
Barkin Aygun <baaygun@adobe.com>
2020-12-29 06:19:57 -0800

    Up to generics

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,16 +7,11 @@
 // Execute the command `rustlings hint collections2` if you need
 // hints.
 
-// I AM NOT DONE
-
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
-    for i in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
-    }
-
+    v.iter().map(|x| x * 2).collect::<Vec<i32>>()
+    
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
-    v
+    
 }
 
 #[cfg(test)]
